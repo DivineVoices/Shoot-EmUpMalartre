0a7c75329de840b67152f126d8a4fec5060837dc
Nyevius <130132864+Nyevius@users.noreply.github.com>
2024-12-03 09:23:48 +0100

    [EDIT] Solution Generator Update

--- config/shootem_up.sol
+++ config/shootem_up.sol
@@ -4,7 +4,5 @@
     "version": "16",
     "version_full": "16.0.30319.14",
     "minimum_version": "10.0.40219.1",
-    "projects": [
-        "shootem_up.prj"
-    ]
+    "projects": "shootem_up.prj"
 }
