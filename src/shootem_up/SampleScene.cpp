af45bc504d2477b4db196820b4a06a8a9c7f01e2
EliasROUSSEAU <elrousseau@gaming.tech>
2024-12-04 14:43:08 +0100

    [EDIT] Mouvement fluide joueur
    SampleScene.cpp
    SampleScene.h

--- src/shootem_up/SampleScene.cpp
+++ src/shootem_up/SampleScene.cpp
@@ -14,25 +14,61 @@ void SampleScene::OnInitialize()
 	pEntity2 = CreateEntity<DummyEntity>(50, sf::Color::Green);
 	pEntity2->SetPosition(500, 500);
 
+	pPlayer = CreateEntity<DummyEntity>(50, sf::Color::White);
+	pPlayer->SetPosition(800, 350);
+
+	x = pPlayer->GetPosition().x;
+	y = pPlayer->GetPosition().y;
+
+	speed = 200;
+
 	pEntitySelected = nullptr;
+
+	mAreas[0] = { 0, 0, 1100, 720 };
+
 }
 
 void SampleScene::OnEvent(const sf::Event& event)
 {
-	if (event.type != sf::Event::EventType::MouseButtonPressed)
-		return;
-
-	if (event.mouseButton.button == sf::Mouse::Button::Right)
-	{
-		TrySetSelectedEntity(pEntity1, event.mouseButton.x, event.mouseButton.y);
-		TrySetSelectedEntity(pEntity2, event.mouseButton.x, event.mouseButton.y);
+	if (event.type == sf::Event::MouseButtonPressed) {
+		if (event.mouseButton.button == sf::Mouse::Button::Left)
+		{
+			if (pEntitySelected != nullptr) 
+			{
+				pEntitySelected->GoToPosition(event.mouseButton.x, event.mouseButton.y, 100.f);
+			}
+		}
+		if (event.mouseButton.button == sf::Mouse::Button::Right)
+		{
+			TrySetSelectedEntity(pEntity1, event.mouseButton.x, event.mouseButton.y);
+			TrySetSelectedEntity(pEntity2, event.mouseButton.x, event.mouseButton.y);
+		}
 	}
 
-	if (event.mouseButton.button == sf::Mouse::Button::Left)
-	{
-		if (pEntitySelected != nullptr) 
-		{
-			pEntitySelected->GoToPosition(event.mouseButton.x, event.mouseButton.y, 100.f);
+	// Mouvement
+	if (event.type == sf::Event::KeyPressed) {
+		if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) {
+			direction.x = 1;
+		}
+		else if (event.key.code == sf::Keyboard::Q || event.key.code == sf::Keyboard::Left) {
+			direction.x = -1;
+		}
+		else if (event.key.code == sf::Keyboard::Z || event.key.code == sf::Keyboard::Up) {
+			direction.y = -1;
+		}
+		else if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down) {
+			direction.y = 1;
+		}
+	}
+
+	if (event.type == sf::Event::KeyReleased) {
+		if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right ||
+			event.key.code == sf::Keyboard::Q || event.key.code == sf::Keyboard::Left) {
+			direction.x = 0;
+		}
+		if (event.key.code == sf::Keyboard::Z || event.key.code == sf::Keyboard::Up ||
+			event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down) {
+			direction.y = 0;
 		}
 	}
 }
@@ -52,4 +88,19 @@ void SampleScene::OnUpdate()
 		sf::Vector2f position = pEntitySelected->GetPosition();
 		Debug::DrawCircle(position.x, position.y, 10, sf::Color::Blue);
 	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		const AABB& aabb = mAreas[i];
+
+		Debug::DrawRectangle(aabb.xMin, aabb.yMin, aabb.xMax - aabb.xMin, aabb.yMax - aabb.yMin, sf::Color::White);
+	}
+
+	float dt = GameManager::Get()->GetDeltaTime();
+	sf::Vector2f velocity = direction * (speed * dt);
+
+	x += velocity.x;
+	y += velocity.y;
+
+	pPlayer->SetPosition(x, y);
 }
--- src/shootem_up/SampleScene.h
+++ src/shootem_up/SampleScene.h
@@ -2,22 +2,40 @@
 
 #include "Scene.h"
 
+#include "iostream"
+
 class DummyEntity;
+
+struct AABB
+{
+	int xMin;
+	int yMin;
+	int xMax;
+	int yMax;
+};
 
 class SampleScene : public Scene
 {
 	DummyEntity* pEntity1;
 	DummyEntity* pEntity2;
+	DummyEntity* pPlayer;
 
 	DummyEntity* pEntitySelected;
 
 private:
 	void TrySetSelectedEntity(DummyEntity* pEntity, int x, int y);
 
+	int x;
+	int y;
+	int speed;
+	sf::Vector2f direction;
+
 public:
 	void OnInitialize() override;
 	void OnEvent(const sf::Event& event) override;
 	void OnUpdate() override;
+
+	AABB mAreas[0];
 };
 
 
