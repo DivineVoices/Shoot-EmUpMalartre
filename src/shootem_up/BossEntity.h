95beac7ecd08dbc2076dc39cc98453736964d1d4
EliasROUSSEAU <elrousseau@gaming.tech>
2024-12-16 13:10:28 +0100

    [EDIT] boss à continuer

--- src/shootem_up/BossEntity.h
+++ src/shootem_up/BossEntity.h
@@ -5,13 +5,19 @@ class BossEntity : public EnemyEntity
 {
 protected:
 	void OnUpdate() override;
-	void Shoot();
+	void Phase1();
+	void Phase2();
+	void Phase3();
 public:
 	void SetTarget(Entity* player) { mPlayer = player; }
+	void BasicShoot();
+	void EventailShoot();
 	BossEntity()
 	{
 		EnemyLife = 900;
 	}
 private:
 	Entity* mPlayer = nullptr;
+	float timeSinceLastBossShot = 0.0f;
+	float timer = 0.0f;
 };
